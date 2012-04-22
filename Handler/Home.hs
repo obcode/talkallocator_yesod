53c424f82e7761b95f409b82fdd5e089978cdd92
Oliver Braun <ob@obraun.net>
2012-04-22 13:51:48 +0200

    Talks can be added to the database

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -3,37 +3,46 @@ module Handler.Home where
 
 import Import
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 getHomeR :: Handler RepHtml
 getHomeR = do
-    (formWidget, formEnctype) <- generateFormPost sampleForm
-    let submission = Nothing :: Maybe (FileInfo, Text)
-        handlerName = "getHomeR" :: Text
+    (formWidget, formEnctype) <- generateFormPost talkForm
+    let handlerName = "getHomeR" :: Text
+    availableTalks <- runDB $ selectList [TalkSpeaker ==. Nothing] [Asc TalkTitle]
+    talksWithSpeaker <- runDB $ selectList [TalkSpeaker !=. Nothing] [Asc TalkTitle]
     defaultLayout $ do
         aDomId <- lift newIdent
-        setTitle "Welcome To Yesod!"
+        setTitle "TalkAllocator!"
         $(widgetFile "homepage")
 
 postHomeR :: Handler RepHtml
 postHomeR = do
-    ((result, formWidget), formEnctype) <- runFormPost sampleForm
+    ((result, _), _) <- runFormPost talkForm
     let handlerName = "postHomeR" :: Text
-        submission = case result of
-            FormSuccess res -> Just res
-            _ -> Nothing
+    case result of
+      FormSuccess talk -> do
+        talkid <- runDB $ insert talk
+        setMessage "Talk added"
+      _ -> do
+        setMessage "Talk could not be added"
+    redirect HomeR
 
+talkForm :: Form Talk
+talkForm = renderBootstrap $ flip Talk Nothing
+    <$> areq textField "Title" Nothing
+
+getDelR :: Handler RepHtml
+getDelR = do
+    (formWidget, formEnctype) <- generateFormPost deleteForm
+    let handlerName = "getHomeR" :: Text
+    talks <- runDB $ selectList [] [Asc TalkTitle]
     defaultLayout $ do
         aDomId <- lift newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
+        setTitle "TalkAllocator!"
+        $(widgetFile "delpage")
 
-sampleForm :: Form (FileInfo, Text)
-sampleForm = renderDivs $ (,)
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField "What's on the file?" Nothing
+deleteForm :: Form Text
+deleteForm = renderBootstrap $ id
+    <$> areq textField "Talks" Nothing
+
+postDelR :: Handler RepHtml
+postDelR = redirect HomeR
